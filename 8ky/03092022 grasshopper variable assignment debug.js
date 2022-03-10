3e0cad9978dab59f8e1a8c810028d16e74799659
Gabriel Miller <gabriel.miller.developer@gmail.com>
2022-03-09 22:12:05 -0800

    added challenge for 3/09/22

---
+++ 8ky/03092022 grasshopper variable assignment debug.js
@@ -0,0 +1,17 @@
+// Variable assignment
+
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+
+// ****** broken ******
+var a == "dev"
+var b == "Lab"
+
+var name == a + b
+
+// ****** fixed ******
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
