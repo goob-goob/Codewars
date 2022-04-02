e2ef740af220725f32a70b8bfe8d1c5701bdfbf7
Gabe Miller <gabriel.miller.developer@gmail.com>
2022-04-01 18:51:09 -0700

    "submitted

---
+++ 8ky/Filtering out the geese 04012022.js
@@ -0,0 +1,5 @@
+// return an array containing all of the strings in the input array except those that match strings in geese
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(bird => !geese.includes(bird))
+  };
