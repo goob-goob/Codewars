d24c77a0858b40e221cc2a0114feae629aca27d9
Gabriel Miller <gabriel.miller.developer@gmail.com>
2022-04-11 00:07:46 -0700

    hopefully this finally fixes that file

--- 8ky/04102022 How good are you really.js
+++ 8ky/04102022 How good are you really.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // <!-- There was a test in your class and you passed it. Congratulations!
 // But you're an ambitious person. You want to know if you're better than the average student in your class.
 
@@ -26,32 +25,3 @@ function betterThanAverage(classPoints, yourPoints) {
 // function betterThanAverage(classPoints, yourPoints) {
 //   return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
 // }
-=======
-// <!-- There was a test in your class and you passed it. Congratulations!
-// But you're an ambitious person. You want to know if you're better than the average student in your class.
-
-// You receive an array with your peers' test scores. Now calculate the average and compare your score!
-
-// Return True if you're better, else False!
-// Note:
-
-// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
-
-//  -->
-
-function betterThanAverage(classPoints, yourPoints) {
-  let average = 0
-
-  Array.prototype.forEach.call(classPoints, element => {
-    average += element
-  })
-  average = average / classPoints.length
-  
-  return yourPoints > average ? true : false
-}
-
-
-// function betterThanAverage(classPoints, yourPoints) {
-//   return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
-// }
->>>>>>> 482687f2ec2d41dca45949e43b71c498639d296e
