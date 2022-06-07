f1e4f45b91606a58f11fe08eaaf8a01d338f0416
Gabriel Miller <gabriel.miller.developer@gmail.com>
2022-06-07 06:18:52 -0700

    added another challenge for 6/7/22

---
+++ 8ky/06072022 is there a vowel in there.js
@@ -0,0 +1,9 @@
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// Return the resulting array.
+
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+const isVow = arr => arr.map((code) => vowels.includes(String.fromCharCode(code)) ? String.fromCharCode(code) : code)
