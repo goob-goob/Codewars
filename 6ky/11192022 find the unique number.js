15665cbd46e5321dc28191c69fc5b59260166186
Gabriel Miller <gabriel.miller.developer@gmail.com>
2022-11-19 12:27:57 -0800

    added challenge for today

---
+++ 6ky/11192022 find the unique number.js
@@ -0,0 +1,18 @@
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+    console.log('run\n')
+    if (arr[0] !== arr[1] && arr[0] !== arr[2]) { return arr[0] }
+    if (arr[arr.length - 1] !== arr[arr.length - 2] && arr[arr.length - 1] !== arr[arr.length - 3]) { return arr[arr.length - 1] }
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1] && arr[i] !== arr[i + 1]) { return arr[i] }
+    }
+}
